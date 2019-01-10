b3efd85cd2560aaf1e9526a1974005fad94d29e6
Abigail Brady <github@morwen.net>
2019-01-10 20:40:41 +0000

    add more tests

--- testRhyme.py
+++ testRhyme.py
@@ -6,11 +6,15 @@ class TestRhyme(unittest.TestCase):
 
 	def test_rhymes(self):
 
-		self.assertTrue(rhyme.ipa_rhymes("tip", "rip"))
-		self.assertFalse(rhyme.ipa_rhymes("tip", "rig"))
-		
 		self.assertTrue(rhyme.rhymes("how", "now"))
 		self.assertFalse(rhyme.rhymes("brown", "cow"))
+		
+		self.assertTrue(rhyme.rhymes("violence", "silence"))
+		self.assertTrue(rhyme.rhymes("me", "me"))
+		self.assertFalse(rhyme.rhymes("arms", "harm"))
+		self.assertTrue(rhyme.rhymes("broken", "spoken"))
+		self.assertFalse(rhyme.rhymes("intense", "trivial"))
+		self.assertTrue(rhyme.rhymes("remain", "pain"))
 	
 		self.assertFalse(rhyme.rhymes("relax", "contracts"))
 		self.assertTrue(rhyme.rhymes("relax", "tacks"))
